fb119c2f7a4a553c8d5e29d1e560f2a4f8792e30
Viraj97 <rathnayaka.viraj@gmail.com>
2021-11-19 22:32:08 +0530

    first

---
+++ src/main/java/com/ivehicle/IvehicleApplication.java
@@ -0,0 +1,13 @@
+package com.ivehicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvehicleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IvehicleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivehicle/IvehicleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivehicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IvehicleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
